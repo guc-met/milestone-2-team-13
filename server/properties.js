89010bf0d14ced6bdd50f03272d129b1ab6ba0e2
Ahmed Mokhtar <ahmed.abdelrehim@student.guc.edu.eg>
2021-01-14 07:53:06 +0200

    deleting old server

--- server/properties.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    CONNECTION_STRING: 'mongodb+srv://admin:admin@omegacentauri.vydfb.mongodb.net/portalData?retryWrites=true&w=majority',
-    PORT: 4000,
-    JWT_KEY: 'akjhfdkadsjh32Efdsjkl78$^&alsdsfajh@#jkhsdfak%jhfdsa'
-};
